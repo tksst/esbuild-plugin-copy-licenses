dfc57ee6246291ab46902a808dee0383463a1fe2
Takashi Sato <takashi@tks.st>
2023-02-10 01:55:52 +0900

    template to package

--- tsup.config.lib.mjs
+++ tsup.config.lib.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     ...libOptions,
     entry: ["src/lib/index.ts"],
+    // ESM only due to read-pkg-up
+    format: ["esm"],
 
     // If you know that this library is for Node.js or for a browser, you may want to set the platform.
     // platform: "node",
