662a3c02e6c2b4e9bcc0493158e5fe033b7563c8
Takashi Sato <takashi@tks.st>
2023-02-10 02:03:12 +0900

    template to package

--- tsup.config.bin.mjs
+++
@@ -1,7 +0,0 @@
-import { binOptions } from "@tksst/typescript-starter-configs/tsup-config.mjs";
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    ...binOptions,
-    entry: ["src/bin/index.ts"],
-});
